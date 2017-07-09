7986ba55cba72c3f47baeed584fcb02fe73013f1
avdgrinten <alexander.vandergrinten@gmail.com>
2017-07-09 16:14:01 +0200

    Implement assertions

--- include/frg/macros.hpp
+++ include/frg/macros.hpp
@@ -1,10 +1,21 @@
 #ifndef FRG_MACROS_HPP
 #define FRG_MACROS_HPP
 
+#define FRG_STRINGIFY(x) #x
+#define FRG_EVALSTRINGIFY(x) FRG_STRINGIFY(x)
+
+#define FRG_INTF(x) frg_ ## x
+
+extern "C" {
+	void FRG_INTF(log)(const char *cstring);
+	void FRG_INTF(panic)(const char *cstring);
+}
+
 // TODO: Switch visibility depending on compilation flags.
 #define FRG_VISIBILITY
 
 // TODO: Actually provide assertions.
-#define FRG_ASSERT(x) ((void)(0))
+#define FRG_ASSERT(x) do { if(!(x)) FRG_INTF(panic)(__FILE__ ":" FRG_EVALSTRINGIFY(__LINE__) \
+		": Assertion '" #x "' failed!"); } while(0)
 
 #endif // FRG_MACROS_HPP
