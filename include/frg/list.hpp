1a78b2881f811861a6bdb85ad46efa7305321f08
avdgrinten <alexander.vandergrinten@gmail.com>
2017-09-08 22:45:56 +0200

    list: Add additional assertions

--- include/frg/list.hpp
+++ include/frg/list.hpp
@@ -17,10 +17,11 @@ struct intrusive_list_hook {
 	using borrow_pointer = BorrowPointer;
 
 	intrusive_list_hook()
-	: next{nullptr}, previous{nullptr} { }
+	: next{nullptr}, previous{nullptr}, in_list{false} { }
 
 	owner_pointer next;
 	borrow_pointer previous;
+	bool in_list;
 };
 
 struct locate_tag { };
@@ -75,6 +76,7 @@ public:
 	};
 
 	iterator iterator_to(borrow_pointer ptr) {
+		FRG_ASSERT(h(ptr).in_list);
 		return iterator{ptr};
 	}
 
@@ -83,7 +85,11 @@ public:
 	
 
 	void push_front(owner_pointer element) {
+		FRG_ASSERT(element);
 		borrow_pointer borrow = traits::decay(element);
+		FRG_ASSERT(!h(borrow).in_list);
+		FRG_ASSERT(!h(borrow).next);
+		FRG_ASSERT(!h(borrow).previous);
 		if(!_front) {
 			_back = borrow;
 		}else{
@@ -91,10 +97,15 @@ public:
 			h(_front).previous = borrow;
 		}
 		_front = std::move(element);
+		h(borrow).in_list = true;
 	}
 	
 	void push_back(owner_pointer element) {
+		FRG_ASSERT(element);
 		borrow_pointer borrow = traits::decay(element);
+		FRG_ASSERT(!h(borrow).in_list);
+		FRG_ASSERT(!h(borrow).next);
+		FRG_ASSERT(!h(borrow).previous);
 		if(!_back) {
 			_front = std::move(element);
 		}else{
@@ -102,6 +113,7 @@ public:
 			h(_back).next = std::move(element);
 		}
 		_back = borrow;
+		h(borrow).in_list = true;
 	}
 
 	bool empty() {
@@ -111,37 +123,49 @@ public:
 	borrow_pointer front() {
 		return _front;
 	}
+	borrow_pointer back() {
+		return _back;
+	}
 
 	owner_pointer pop_front() {
-		return erase(iterator_to(_front));
+		FRG_ASSERT(h(_front).in_list);
+		return erase(iterator{_front});
 	}
 	owner_pointer pop_back() {
-		return erase(iterator_to(_back));
+		FRG_ASSERT(h(_back).in_list);
+		return erase(iterator{_back});
 	}
 
 	owner_pointer erase(iterator it) {
+		FRG_ASSERT(it._current);
+		FRG_ASSERT(h(it._current).in_list);
 		owner_pointer next = std::move(h(it._current).next);
 		borrow_pointer previous = h(it._current).previous;
 
 		if(!next) {
+			FRG_ASSERT(_back == it._current);
 			_back = previous;
 		}else{
+			FRG_ASSERT(h(traits::decay(next)).previous == it._current);
 			h(traits::decay(next)).previous = previous;
 		}
 
 		owner_pointer erased;
 		if(!previous) {
+			FRG_ASSERT(traits::decay(_front) == it._current);
 			erased = std::move(_front);
 			_front = std::move(next);
 		}else{
+			FRG_ASSERT(traits::decay(h(previous).next) == it._current);
 			erased = std::move(h(previous).next);
 			h(previous).next = std::move(next);
 		}
 
+		FRG_ASSERT(traits::decay(erased) == it._current);
 		h(it._current).next = nullptr;
 		h(it._current).previous = nullptr;
+		h(it._current).in_list = false;
 
-		FRG_ASSERT(traits::decay(erased) == it._current);
 		return erased;
 	}
 
