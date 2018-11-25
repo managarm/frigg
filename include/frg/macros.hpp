c4e6ca80e76e279c89c4275106eef0abc3a9ea8b
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2018-11-25 11:59:54 +0100

    macros: Make panic function weak

--- include/frg/macros.hpp
+++ include/frg/macros.hpp
@@ -8,14 +8,20 @@
 
 extern "C" {
 	void FRG_INTF(log)(const char *cstring);
-	void FRG_INTF(panic)(const char *cstring);
+	[[gnu::weak]] void FRG_INTF(panic)(const char *cstring);
 }
 
 // TODO: Switch visibility depending on compilation flags.
 #define FRG_VISIBILITY
 
 // TODO: Actually provide assertions.
-#define FRG_ASSERT(x) do { if(!(x)) FRG_INTF(panic)(__FILE__ ":" FRG_EVALSTRINGIFY(__LINE__) \
-		": Assertion '" #x "' failed!"); } while(0)
+#define FRG_ASSERT(x) do { \
+	if(!(x)) { \
+		if(!FRG_INTF(panic)) \
+			__builtin_trap(); \
+		FRG_INTF(panic)(__FILE__ ":" FRG_EVALSTRINGIFY(__LINE__) \
+				": Assertion '" #x "' failed!"); \
+	}\
+} while(0)
 
 #endif // FRG_MACROS_HPP
