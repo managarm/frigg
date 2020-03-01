787489c9c7834c465eb71c95e29d3bd209e29604
Kacper Słomiński <kacper.slominski72@gmail.com>
2020-03-01 12:11:30 +0100

    unique: add unique_ptr implementation

---
+++ include/frg/unique.hpp
@@ -0,0 +1,87 @@
+#ifndef FRG_UNIQUE_HPP
+#define FRG_UNIQUE_HPP
+
+#include <utility>
+
+namespace frg {
+
+template <typename T, typename Allocator>
+struct unique_ptr {
+	friend void swap(unique_ptr &a, unique_ptr &b) {
+		using std::swap;
+		swap(a._ptr, b._ptr);
+		swap(a._allocator, b._allocator);
+	}
+
+	unique_ptr(Allocator allocator)
+	:_ptr{nullptr}, _allocator(std::move(allocator)) {}
+
+	unique_ptr(Allocator allocator, T *ptr)
+	:_ptr{ptr}, _allocator(std::move(allocator)) {}
+
+	~unique_ptr() {
+		if (_ptr)
+			_allocator.free(_ptr);
+	}
+
+	unique_ptr(const unique_ptr &) = delete;
+	unique_ptr &operator=(const unique_ptr &) = delete;
+
+	unique_ptr(unique_ptr &&p) {
+		swap(*this, p);
+	}
+
+	unique_ptr &operator=(unique_ptr &&p) {
+		swap(*this, p);
+		return *this;
+	}
+
+	T *get() {
+		return _ptr;
+	}
+
+	T &operator*() {
+		return *_ptr;
+	}
+
+	T *operator->() {
+		return _ptr;
+	}
+
+	void release() {
+		T *old = _ptr;
+		_ptr = nullptr;
+
+		if (old)
+			_allocator.free(_ptr);
+	}
+
+	operator bool() {
+		return _ptr;
+	}
+
+	void reset(T *ptr) {
+		T *old = _ptr;
+		_ptr = ptr;
+
+		if (old)
+			_allocator.free(old);
+	}
+
+private:
+	T *_ptr;
+	Allocator _allocator;
+};
+
+template <typename T, typename Allocator, typename ...Args>
+unique_ptr<T, Allocator> make_unique(Allocator allocator, Args &&...args) {
+	return unique_ptr<T, Allocator>{
+		std::move(allocator),
+		new (allocator.allocate(sizeof(T))) T{
+			std::forward<Args>(args)...}
+	};
+}
+
+}
+
+#endif //FRG_UNIQUE_HPP
