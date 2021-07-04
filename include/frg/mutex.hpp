6e467956709c54042ba102ce11d32810211998be
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2021-07-04 22:40:26 +0200

    mutex: Add shared_lock

--- include/frg/mutex.hpp
+++ include/frg/mutex.hpp
@@ -36,7 +36,7 @@ public:
 	}
 
 	unique_lock(const unique_lock &other) = delete;
-	
+
 	unique_lock(unique_lock &&other)
 	: unique_lock() {
 		swap(*this, other);
@@ -61,6 +61,71 @@ public:
 	void unlock() {
 		FRG_ASSERT(_is_locked);
 		_mutex->unlock();
+		_is_locked = false;
+	}
+
+	bool is_locked() {
+		return _is_locked;
+	}
+
+	bool protects(Mutex *mutex) {
+		return _is_locked && mutex == _mutex;
+	}
+
+private:
+	Mutex *_mutex;
+	bool _is_locked;
+};
+
+template<typename Mutex>
+class shared_lock {
+public:
+	friend void swap(shared_lock &u, shared_lock &v) {
+		using std::swap;
+		swap(u._mutex, v._mutex);
+		swap(u._is_locked, v._is_locked);
+	}
+
+	shared_lock()
+	: _mutex{nullptr}, _is_locked{false} { }
+
+	shared_lock(dont_lock_t, Mutex &mutex)
+	: _mutex{&mutex}, _is_locked{false} { }
+
+	shared_lock(adopt_lock_t, Mutex &mutex)
+	: _mutex{&mutex}, _is_locked{true} { }
+
+	shared_lock(Mutex &mutex)
+	: _mutex{&mutex}, _is_locked{false} {
+		lock();
+	}
+
+	shared_lock(const shared_lock &other) = delete;
+
+	shared_lock(shared_lock &&other)
+	: shared_lock() {
+		swap(*this, other);
+	}
+
+	~shared_lock() {
+		if(_is_locked)
+			unlock();
+	}
+
+	shared_lock &operator= (shared_lock other) {
+		swap(*this, other);
+		return *this;
+	}
+
+	void lock() {
+		FRG_ASSERT(!_is_locked);
+		_mutex->lock_shared();
+		_is_locked = true;
+	}
+
+	void unlock() {
+		FRG_ASSERT(_is_locked);
+		_mutex->unlock_shared();
 		_is_locked = false;
 	}
 
