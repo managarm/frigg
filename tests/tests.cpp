b039d78432ba21f7734c525cbd7f739eaffa23e7
Arsen Arsenović <arsen@aarsen.me>
2022-06-17 23:33:18 +0200

    tests: add test suite

---
+++ tests/tests.cpp
@@ -0,0 +1,33 @@
+#include <frg/string.hpp>
+#include <frg/std_compat.hpp>
+
+#include <gtest/gtest.h>
+
+using string = frg::string<frg::stl_allocator>;
+
+namespace {
+void common_startsends_tests(auto ts1) {
+	EXPECT_TRUE(ts1.starts_with("abc"));
+	EXPECT_TRUE(ts1.ends_with("def"));
+
+	EXPECT_FALSE(ts1.starts_with("def"));
+	EXPECT_FALSE(ts1.ends_with("abc"));
+
+	EXPECT_FALSE(ts1.ends_with("this long string should not crash"));
+
+	EXPECT_TRUE(ts1.starts_with(ts1));
+	EXPECT_TRUE(ts1.ends_with(ts1));
+}
+}
+
+TEST(strings, string_starts_ends_with) {
+	string ts1 { "abc123def" };
+
+	common_startsends_tests(ts1);
+}
+
+TEST(strings, view_starts_ends_with) {
+	frg::string_view ts1 { "abc123def" };
+
+	common_startsends_tests(ts1);
+}
