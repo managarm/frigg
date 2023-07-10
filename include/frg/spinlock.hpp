d29b8d18c24f2857c2eae6c8464d249162df7262
Yusuf <94141127+cleanbaja@users.noreply.github.com>
2023-07-10 12:16:22 -0500

    spinlock: add a regular spinlock
    Also added spinloop hints to make the spinlocks more efficient

--- include/frg/spinlock.hpp
+++ include/frg/spinlock.hpp
@@ -4,6 +4,29 @@
 
 namespace frg {
 
+namespace detail {
+
+[[gnu::always_inline]] inline void loophint() {
+#if defined(__x86_64__) || defined(__i386__)
+	// use the dedicated `pause` instruction
+	__builtin_ia32_pause();
+#elif defined(__aarch64__)
+	//
+	// `isb` seems more reliable for than `yield` for releasing
+	// resources on modern aarch64 CPUs
+	//
+	// https://bugs.java.com/bugdatabase/view_bug.do?bug_id=8258604
+	// https://bugs.mysql.com/bug.php?id=100664
+	//
+	asm volatile ("isb" ::: "memory");
+#else
+	// otherwise, use a standard memory barrier
+	asm volatile ("" ::: "memory");
+#endif
+}
+
+} // namespace detail
+
 struct ticket_spinlock {
 	constexpr ticket_spinlock()
 	: next_ticket_{0}, serving_ticket_{0} { }
@@ -13,8 +36,14 @@ struct ticket_spinlock {
 
 	void lock() {
 		auto ticket = __atomic_fetch_add(&next_ticket_, 1, __ATOMIC_RELAXED);
-		while(__atomic_load_n(&serving_ticket_, __ATOMIC_ACQUIRE) != ticket)
-			;
+		while(__atomic_load_n(&serving_ticket_, __ATOMIC_ACQUIRE) != ticket) {
+			detail::loophint();
+		}
+	}
+
+	bool is_locked() {
+		return (__atomic_load_n(&serving_ticket_, __ATOMIC_RELAXED) + 1)
+			== __atomic_load_n(&next_ticket_, __ATOMIC_RELAXED);
 	}
 
 	void unlock() {
@@ -25,6 +54,37 @@ struct ticket_spinlock {
 private:
 	uint32_t next_ticket_;
 	uint32_t serving_ticket_;
+};
+
+struct simple_spinlock {
+	constexpr simple_spinlock()
+	: lock_{false} { }
+
+	simple_spinlock(const simple_spinlock &) = delete;
+	simple_spinlock &operator= (const simple_spinlock &) = delete;
+
+	void lock() {
+		while (true) {
+			if (!__atomic_exchange_n(&lock_, true, __ATOMIC_ACQUIRE)) {
+				return;
+			}
+
+			while (__atomic_load_n(&lock_, __ATOMIC_RELAXED)) {
+				detail::loophint();
+			}
+		}
+	}
+
+	bool is_locked() {
+		return __atomic_load_n(&lock_, __ATOMIC_RELAXED);
+	}
+
+	void unlock() {
+		__atomic_store_n(&lock_, false, __ATOMIC_RELEASE);
+	}
+
+private:
+	bool lock_;
 };
 
 } // namespace frg
