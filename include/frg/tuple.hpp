a1a63797cad7b4df5e84bd54ac21f29012c33763
Qwinci <qwinci222@gmail.com>
2025-12-31 16:59:23 +0200

    tuple: make more functions constexpr

--- include/frg/tuple.hpp
+++ include/frg/tuple.hpp
@@ -16,17 +16,17 @@ namespace _tuple {
 
 	template<typename T, typename... Types>
 	struct storage<T, Types...> {
-		storage() = default;
+		constexpr storage() = default;
 
-		storage(T item, Types... tail)
+		constexpr storage(T item, Types... tail)
 		: item(std::forward<T>(item)), tail(std::forward<Types>(tail)...) { }
 
 		template<typename... UTypes>
-		storage(const storage<UTypes...> &other)
+		constexpr storage(const storage<UTypes...> &other)
 		: item(other.item), tail(other.tail) { }
 
 		template<typename... UTypes>
-		storage(storage<UTypes...> &&other)
+		constexpr storage(storage<UTypes...> &&other)
 		: item(std::move(other.item)), tail(std::move(other.tail)) { }
 
 		T item;
@@ -56,10 +56,10 @@ namespace _tuple {
 
 	template<int n, typename T, typename... Types>
 	struct access_helper<n, T, Types...> {
-		static typename nth_type<n - 1, Types...>::type &access(storage<T, Types...> &stor) {
+		static constexpr typename nth_type<n - 1, Types...>::type &access(storage<T, Types...> &stor) {
 			return access_helper<n - 1, Types...>::access(stor.tail);
 		}
-		static const typename nth_type<n - 1, Types...>::type &access(
+		static constexpr const typename nth_type<n - 1, Types...>::type &access(
 				const storage<T, Types...> &stor) {
 			return access_helper<n - 1, Types...>::access(stor.tail);
 		}
@@ -67,10 +67,10 @@ namespace _tuple {
 
 	template<typename T, typename... Types>
 	struct access_helper<0, T, Types...> {
-		static T &access(storage<T, Types...> &stor) {
+		static constexpr T &access(storage<T, Types...> &stor) {
 			return stor.item;
 		}
-		static const T &access(const storage<T, Types...> &stor) {
+		static constexpr const T &access(const storage<T, Types...> &stor) {
 			return stor.item;
 		}
 	};
@@ -80,9 +80,9 @@ namespace _tuple {
 template<typename... Types>
 class tuple {
 public:
-	tuple() = default;
+	constexpr tuple() = default;
 
-	tuple(Types... args)
+	constexpr tuple(Types... args)
 	: _stor(std::forward<Types>(args)...) { }
 
 	template<typename... UTypes>
@@ -106,19 +106,19 @@ public:
 	template<typename... UTypes,
 		typename = std::enable_if_t<_tuple_is_constructible<
 			sizeof...(UTypes) - 1, const UTypes &...>::value>>
-	tuple(const tuple<UTypes...> &other) : _stor(other._stor) { }
+	constexpr tuple(const tuple<UTypes...> &other) : _stor(other._stor) { }
 
 	template<typename... UTypes,
 		typename = std::enable_if_t<_tuple_is_constructible<
 			sizeof...(UTypes) - 1, UTypes &&...>::value>>
-	tuple(tuple<UTypes...> &&other) : _stor(std::move(other._stor)) { }
+	constexpr tuple(tuple<UTypes...> &&other) : _stor(std::move(other._stor)) { }
 
 	template<int n>
-	typename _tuple::nth_type<n, Types...>::type &get() {
+	constexpr typename _tuple::nth_type<n, Types...>::type &get() {
 		return _tuple::access_helper<n, Types...>::access(_stor);
 	}
 	template<int n>
-	const typename _tuple::nth_type<n, Types...>::type &get() const {
+	constexpr const typename _tuple::nth_type<n, Types...>::type &get() const {
 		return _tuple::access_helper<n, Types...>::access(_stor);
 	}
 
@@ -131,7 +131,7 @@ template<>
 class tuple<> { };
 
 template<typename... Types>
-tuple<typename std::remove_reference_t<Types>...> make_tuple(Types &&... args) {
+constexpr tuple<typename std::remove_reference_t<Types>...> make_tuple(Types &&... args) {
 	 // TODO(arsen): transform reference_wrappers into lvalue references
 	 // when they get added to cxxshim
 	return tuple<typename std::remove_reference_t<Types>...>(std::forward<Types>(args)...);
@@ -139,12 +139,12 @@ tuple<typename std::remove_reference_t<Types>...> make_tuple(Types &&... args) {
 
 namespace _tuple {
 	template<typename F, typename... Args, size_t... I>
-	auto apply(F functor, const tuple<Args...> &args, std::index_sequence<I...>) {
+	constexpr auto apply(F functor, const tuple<Args...> &args, std::index_sequence<I...>) {
 		return functor(args.template get<I>()...);
 	}
 
 	template<typename F, typename... Args, size_t... I>
-	auto apply(F functor, tuple<Args...> &&args, std::index_sequence<I...>) {
+	constexpr auto apply(F functor, tuple<Args...> &&args, std::index_sequence<I...>) {
 		return functor(std::move(args.template get<I>())...);
 	}
 
@@ -239,18 +239,18 @@ namespace _tuple {
 } // namespace tuple
 
 template<typename F, typename... Args>
-auto apply(F functor, const tuple<Args...> &args) {
+constexpr auto apply(F functor, const tuple<Args...> &args) {
 	return _tuple::apply(std::move(functor), args, std::index_sequence_for<Args...>());
 }
 
 template<typename F, typename... Args>
-auto apply(F functor, tuple<Args...> &&args) {
+constexpr auto apply(F functor, tuple<Args...> &&args) {
 	return _tuple::apply(std::move(functor), std::move(args), std::index_sequence_for<Args...>());
 }
 
 template <typename... Tuples,
 	 typename Ret = typename _tuple::tuple_cat_result<Tuples...>::type>
-Ret tuple_cat(Tuples&&... args) {
+constexpr Ret tuple_cat(Tuples&&... args) {
 	typedef typename _tuple::make_indices_from_1st<Tuples...>::type index;
 	typedef _tuple::tuple_concater<Ret, index, Tuples...> concater;
 	return concater::do_concat(std::forward<Tuples>(args)...);
